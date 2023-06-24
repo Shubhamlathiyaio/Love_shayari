5b49a82421109c51af7de1e8e1afe92175414455
harmit <flutterdeveloper888@gmail.com>
2023-06-24 12:47:01 +0530

    Completed till View.

--- lib/view.dart
+++ lib/view.dart
@@ -6,8 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:love_shayari/Data.dart';
 
 class view extends StatefulWidget {
-List a;
-int index;
+  List a;
+  int index;
 
   view(this.a, this.index);
 
@@ -18,83 +18,176 @@ int index;
 class _viewState extends State<view> {
   @override
   List shayari = [];
-  bool colortf=false;
-  List<Color> co=Data.co;
-  int R=0;
+  List<Color> co = [Colors.pink, Colors.yellow];
+  int R = 0;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-
+    //co=Data.co.shuffle();
   }
 
-  rando()
-  {
-    R=Random().nextInt(5);
+  random() {
+    do {
+      R = Random().nextInt(5);
+    } while (R == 0 || R == 1);
+    co = Data.co;
     co.shuffle();
-    co=co.sublist(0,2);
+    co = co.sublist(0, R);
     return co;
   }
+
   Widget build(BuildContext context) {
-    // List<Color> co=rando();
+    double size=(MediaQuery.of(context).size.height)-(MediaQuery.of(context).padding.top)-(kToolbarHeight)-10;
+
     return Scaffold(
-        appBar: AppBar(backgroundColor: Colors.green,title: Text("Love Shayari",style: TextStyle(fontSize: 24),),),
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text(
+            "Love Shayari",
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
         body: Column(
           children: [
-            SizedBox(height: 10,),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                IconButton(onPressed: () {
-                  showModalBottomSheet(
-                      context: context,
-                      builder: (context) {
-                        return Container();
-                      });
-                }, icon: Icon(Icons.color_lens,size: 40,)),
-                Text("${widget.index+1} / ${widget.a.length}",style: TextStyle(fontSize: 24)),
-                IconButton(onPressed: () {
-                  colortf=true;
-                  co=rando();
-                  setState(() {});
-                }, icon: Icon(Icons.refresh_rounded,size: 40,)),
+                IconButton(
+                    onPressed: () {
+                      showModalBottomSheet(
+                          isScrollControlled: true,
+                          context: context,
+                          builder: (context) {
+                            return Container(
+                            height: size,
+                            child: GridView.builder(
+                                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 2
+                                ),
+                                itemCount: Data.co.length-1,
+                                itemBuilder: (context, index) {
+                                  return InkWell(onTap: () {
+                                    Navigator.pop(context);
+                                    co=[Data.co[index],Data.co[index+1]];
+                                    setState(() {});
+                                  },
+                                    child: Container(margin: EdgeInsets.all(10),
+                                      decoration: BoxDecoration(gradient: LinearGradient(colors: [Data.co[index],Data.co[index+1]])),
+                                    ),
+                                  );
+                                },
+                            ),
+                            );
+                          },
+                      );
+                    },
+                     icon: Icon(Icons.palette,size: 50,)),
+                Text("${widget.index + 1} / ${widget.a.length}",
+                    style: TextStyle(fontSize: 24)),
+                IconButton(
+                    onPressed: () {
+                      co = random();
+                      setState(() {});
+                    },
+                    icon: Icon(
+                      Icons.refresh_rounded,
+                      size: 40,
+                    )),
               ],
             ),
-            SizedBox(height: 30,),
-            Expanded(child: Container(padding: EdgeInsets.all(20),alignment: Alignment.center,decoration: BoxDecoration(
-              gradient: LinearGradient(colors: colortf ? co : [Colors.pink,Colors.yellow])
+            SizedBox(
+              height: 30,
             ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(20),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors:  co )),
                 child: PageView.builder(
                   onPageChanged: (value) {
-                    widget.index=value;
+                    widget.index = value;
                     setState(() {});
                   },
                   controller: PageController(
-                    initialPage: widget.index,),
+                    initialPage: widget.index,
+                  ),
                   itemCount: widget.a.length,
                   itemBuilder: (context, index) {
-                    return Center(child: Text(widget.a[widget.index],style: TextStyle(fontSize: 36),));
+                    return Center(
+                        child: Text(
+                      widget.a[widget.index],
+                      style: TextStyle(fontSize: 36),
+                    ));
                   },
-                ),),
+                ),
+              ),
             ),
-            SizedBox(height: 50,),
-            Container(height: 70,margin:EdgeInsets.all(10),width: double.infinity, color: Colors.blueGrey,
-              child: Wrap(alignment: WrapAlignment.spaceAround,children: [
-                IconButton(onPressed: () {
-                    widget.index>0 ? widget.index-- : widget.index;
-                    setState(() {});
-                  }, icon: Icon(Icons.arrow_back_ios_new_rounded,size: 50,)),
-                IconButton(onPressed: () {
-                  widget.index>0 ? widget.index-- : widget.index;
-                  setState(() {});
-                }, icon: Icon(Icons.edit,size: 50,)),
-                IconButton(onPressed: () {
-                  widget.index<widget.a.length ? widget.index++ : widget.index;
-                  setState(() {});
-                }, icon: Icon(Icons.arrow_forward_ios_sharp,size: 50,)),
-                ],
-            )),
-            SizedBox(height: 50,),
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+                height: 70,
+                width: double.infinity,
+                color: Colors.green,
+                child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround,crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                        },
+                        icon: Icon(
+                          Icons.copy,
+                          size: 50,
+                        )),
+                    IconButton(
+                        onPressed: () {
+                          widget.index > 0 ? widget.index-- : widget.index;
+                          setState(() {});
+                        },
+                        icon: Icon(
+                          Icons.arrow_back_ios_new_rounded,
+                          size: 50,
+                        )),
+                    IconButton(
+                        onPressed: () {
+                          widget.index > 0 ? widget.index-- : widget.index;
+                          setState(() {});
+
+                        },
+                        icon: Icon(
+                          Icons.edit,
+                          size: 50,
+                        )),
+                    IconButton(
+                        onPressed: () {
+                          widget.index + 1 < widget.a.length
+                              ? widget.index++
+                              : widget.index;
+                          setState(() {});
+                        },
+                        icon: Icon(
+                          Icons.arrow_forward_ios_sharp,
+                          size: 50,
+                        )),
+                    IconButton(
+                        onPressed: () {
+
+                        },
+                        icon: Icon(
+                          Icons.share,
+                          size: 50,
+                        )),
+                  ],
+                )),
+            SizedBox(
+              height: 50,
+            ),
           ],
         ));
   }
